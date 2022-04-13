cd2ed3bf75ecde3d9500f766960b1fa9867b40e9
giorgio <giorgiolink94@hotmail.it>
2022-04-13 20:28:11 +0100

    Update

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import { Col, Row, Button } from "react-bootstrap";
 import posts from "../../../data/posts.json"; //change with connection to server
 import BlogItem from "../blog-item/BlogItem";
+import sgMail from "@sendgrid/mail";
 
 const BlogList = (props) => {
+  const DownloadPdf = async () => {
+    try {
+      await fetch("http://localhost:3000/pdf/DownloadPdf", {
+        headers: {
+          method: "GET",
+        },
+      });
+    } catch (error) {}
+  };
+
   return (
     <Row>
       {posts.map((post) => (
@@ -14,7 +25,7 @@ const BlogList = (props) => {
           }}
         >
           <BlogItem key={post.title} {...post} />
-          <Button>Download PDF</Button>
+          <Button onClick={DownloadPdf}>Download PDF</Button>
         </Col>
       ))}
     </Row>
